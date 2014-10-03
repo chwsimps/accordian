db4753a9e81bc269af4d2b2d9ae36a01125cbff1
Chase Simpson <chase.w.simpson1@gmail.com>
2014-10-03 14:37:17 -0400

    updates to js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,17 @@
-$('.box1').click(function() {
-  $(this).addClass('open');
+$('.head1').click(function() {
+  $('.div1').addClass('open').removeClass('close');
+  $('.div2').addClass('close').removeClass('open');
+  $('.div3').addClass('close').removeClass('open');
+});
+
+$('.head2').click(function() {
+  $('.div1').addClass('close').removeClass('open');
+  $('.div2').addClass('open').removeClass('close');
+  $('.div3').addClass('close').removeClass('open');
+});
+
+$('.head3').click(function() {
+  $('.div1').addClass('close').removeClass('open');
+  $('.div2').addClass('close').removeClass('open');
+  $('.div3').removeClass('close').addClass('open');
 });
